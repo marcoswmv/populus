e1262428c7226bb210b2ced46dca8836386413b1
Marcos Vicente <marcoswmvicente@gmail.com>
2024-11-14 21:23:24 +0000

    Initial Commit

---
+++ Populus/PopulusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopulusApp.swift
+//  Populus
+//
+//  Created by Marcos Vicente on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PopulusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
